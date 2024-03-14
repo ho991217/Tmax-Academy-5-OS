1ace9ae2879348699fc17356129c238057ce2229
ho991217 <ho991217@kakao.com>
2024-03-14 01:24:17 +0000

    stack

---
+++ memory/stack.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void) {
+  int64_t apple = 3;
+  int64_t orange = 7;
+  int64_t name[4] = {1, 2, 3, 4};
+
+  printf("apple address: %p\n", &apple);
+  printf("orange address: %p\n", &orange);
+  printf("name address: %p\n", name);
+
+  return 0;
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+const char *filename = "HELLO WORLD";
+
+int main() {
+  int a = 3;
+  return 0;
+}
